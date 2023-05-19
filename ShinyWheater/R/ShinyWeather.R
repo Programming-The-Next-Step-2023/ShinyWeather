64354de7d8b8e2474d8c0c1e929a45c0af7aa3f3
MagdaMatetovici <123730197+MagdaMatetovici@users.noreply.github.com>
2023-05-19 08:32:01 +0200

    Added documentation to package

--- ShinyWheater/R/ShinyWeather.R
+++ ShinyWheater/R/ShinyWeather.R
@@ -1,9 +1,32 @@
-#' This package makes a Shiny App that shows the weather in the Netherlands in an 
-#' intuitive way for children
-#'
+#' A Child-Friendly Weather App  for the Netherlands
+#' 
+#' 
 #' @section Shiny App:
-#' \emph{ShinyWeather} contains the code for the application
-#'
+#' The \emph{ShinyWeather} app can be run using the runShinyWeather() function.
+#' 
+#' This App is meant to be used by parents together with children. It helps explain the weather to children in a simple way. 
+#' 
+#' @section The display contains:
+#'  * a \emph{map of the Netherlands}
+#'  
+#'  * a \emph{calendar field}
+#'  
+#'  * a \emph{"Show Weather" button}
+#'  
+#'  * a \emph{"Show Activity" button}
+#' 
+#' @section Usage:
+#' First choose a location by clicking on the map and a date by selecting one in the calendar field.
+#' It is possible to check the weather forcast for up to seven days including the current day.  
+#' 
+#' By clicking "Show Weather" a field containing weather variables will appear.
+#' The weather variables displayed are: \emph{temperature},  \emph{rain},  \emph{showers},  \emph{snow} and \emph{wind speed}.
+#' 
+#' By clicking "Show Activities" a field containing images of activties will be displayed.
+#' Browse through the activities using the "Back" and "Forward" buttons.
+#' The activities displayed are based on the weather variables. They show what kind of activities can be done based on the weather conditions of the chosen day.
+#' 
+#' 
 #' @docType package
 #'
 #' @name ShinyWeather
