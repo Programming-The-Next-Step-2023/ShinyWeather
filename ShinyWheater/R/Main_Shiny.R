c17adc3f5064065c15147ab6ef6a65097870e514
MagdaMatetovici <123730197+MagdaMatetovici@users.noreply.github.com>
2023-05-23 19:37:13 +0200

    Added comments to the code

--- ShinyWheater/R/Main_Shiny.R
+++ ShinyWheater/R/Main_Shiny.R
@@ -5,16 +5,23 @@ source("R/weather.R")
 # library(tidyverse)
 
 #library(shinydashboard)
-# Define UI for application that draws a histogram
+
+
 ui <- shiny::fluidPage(
   
   # Application title
   shiny::titlePanel("Shiny Weather App"),
   
-  # Sidebar with a slider input for number of bins 
+  # Sidebar with calendar, weather values and clothing advice
   shiny::sidebarLayout(
+    
     shiny::sidebarPanel(
+  
+      #Calendar Input with only 7 days ahead available
       shiny::dateInput("date", "Select a date:", value = Sys.Date(), min = Sys.Date(), max = Sys.Date()+6),
+      shiny::br(),
+      
+      #Input button to show the weather 
       shiny::actionButton("go_button", "Show Weather"),
       shiny::br(),
       shiny::strong("Temperature (°C):"),
@@ -26,6 +33,8 @@ ui <- shiny::fluidPage(
       shiny::strong("Wind Speed"),
       shiny::textOutput("Wind"),
       shiny::br(),
+      
+      #Input button to show the clothing advice
       shiny::actionButton("go_clothes", "How to dress?"),
       shiny::br(),
       shiny::br(),
@@ -33,11 +42,17 @@ ui <- shiny::fluidPage(
       
     ),
     
-    # Show a plot of the generated distribution
+    # Show a map and a collection of activities
     shiny::mainPanel(
+      
+      #Output which date was selected 
       shiny::textOutput("selectedDate"),
+      
+      #Output a map of the world
       leaflet::leafletOutput("map", width = "100%", height = 400),
       shiny::br(),
+      
+      #Activities button and images that can be browsed through
       shiny::actionButton("show", "Show Activities"),
       shiny::imageOutput("image"),
       shiny::div(
@@ -50,12 +65,15 @@ ui <- shiny::fluidPage(
   )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic required for the app 
+
 server <- function(input, output) {
   
+  #We start with a reactive value for the weathervalues, which
+  # will be changed when we select a location and a day
   weather_data_RT <- reactiveVal(NULL)
   
-  # this outputs how to dress 
+  # this outputs how to dress when clicking "How to dress?" 
   shiny::observeEvent(input$go_clothes, {
     output$dress <- shiny::renderImage({
       list(src = "R/www/winter_clothes.jpg",
@@ -65,16 +83,17 @@ server <- function(input, output) {
     }, deleteFile = FALSE)
   })
   
+  #This prints the latitude and longitude of where we clicked, it is a check 
   shiny::observeEvent(input$map_click, {
     print(input$map_click)
   })
   
-  #this outputs the map
+  #This outputs the map with default to Amsterdam
   output$map <- leaflet::renderLeaflet({
       leaflet::setView(lng = 4.89, lat = 52.37, zoom = 7, leaflet::addTiles(leaflet::leaflet())) # Set the initial view to focus on the Netherlands
   })
   
-  #this outputs a test saying which date was selected 
+  #This outputs a text saying which date was selected 
   output$selectedDate <- shiny::renderText({
     
     # can do some logic with the date here...
@@ -89,12 +108,17 @@ server <- function(input, output) {
   #we start with a currentImageIndex on the first image
   currentImageIndex <- shiny::reactiveVal(1)
   
+  # We define a list of images, they are generated with the find_activities function
+  # Because the input in the find_activities function changes when we set location and time
+  # We set images to be a reactive function
   images <- shiny::reactive({
+    #if the weather data has values 
     if (!is.null(weather_data_RT())) {
       
       # set index back to 1
       currentImageIndex(1)
       
+      #look for activities
       activities <- find_activities(temp = weather_data_RT()$Temp, rain_shower = weather_data_RT()$Rain, snow = weather_data_RT()$Snow, wind = weather_data_RT()$Wind)
       
       # check if we found any activities
@@ -149,7 +173,7 @@ server <- function(input, output) {
     
   })
 
-  #when Go button is pressed, we show weather variables for day and location (day and location to be implemented)
+  #when Go button is pressed, we show weather variables for day and location 
   shiny::observeEvent(input$go_button, {
     
     # obtain day_index from selected date
