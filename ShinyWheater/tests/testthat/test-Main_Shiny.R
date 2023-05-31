f1de5b2981f8655cc800a95072a92f2bb0321d1b
MagdaMatetovici <123730197+MagdaMatetovici@users.noreply.github.com>
2023-05-31 21:28:23 +0200

    Added day_index test

--- ShinyWheater/tests/testthat/test-Main_Shiny.R
+++ ShinyWheater/tests/testthat/test-Main_Shiny.R
@@ -1,3 +1,9 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+#Test for get_day_index()
+test_that("get_day_index() returns correct index", {
+  
+  # index should be 3 for 3 days added to current date
+  test_data <- Sys.Date() + 3
+  
+  index <- get_day_index(test_data)
+  expect_equal(index, index)
 })
