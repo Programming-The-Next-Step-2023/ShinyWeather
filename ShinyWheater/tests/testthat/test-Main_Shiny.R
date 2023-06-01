b18060e4b65dee8eb2201e24938f855ee5a9d52a
MagdaMatetovici <123730197+MagdaMatetovici@users.noreply.github.com>
2023-06-01 18:19:17 +0200

    Added tests for normalize_value()

--- ShinyWheater/tests/testthat/test-Main_Shiny.R
+++ ShinyWheater/tests/testthat/test-Main_Shiny.R
@@ -7,3 +7,17 @@ test_that("get_day_index() returns correct index", {
   index <- get_day_index(test_data)
   expect_equal(index, index)
 })
+
+#Test for normalize_value()
+test_that("normalize_value() returns correct value", {
+  
+  #test if it returns 0 if value falls out of lower range
+  expect_equal(normalize_value(-10, 0, 10), 0)
+  
+  #test if it returns 100 if value falls out of uper range
+  expect_equal(normalize_value(200, 0, 10), 100)
+  
+  #test some simple cases
+  expect_equal(normalize_value(10, -15, 35), 50)
+  expect_equal(normalize_value(10, 0, 10), 10)
+})
