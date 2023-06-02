936f6377a2d19f06a8a3005d0b425579757c05e3
MagdaMatetovici <123730197+MagdaMatetovici@users.noreply.github.com>
2023-06-02 18:58:11 +0200

    Updated documentation

--- R/app.R
+++ R/app.R
@@ -1,7 +1,8 @@
 source("R/Main_shiny.R")
 #' runShinyWeather
 #'
-#' This is the function that can be run by the user to open the ShinyWeather App
+#' This is the function that can be run by the user to open the ShinyWeather App.
+#' For the documentation of the app, use '?ShinyWeather'
 #'
 #' @export
 #'
